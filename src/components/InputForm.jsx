fb24ceaf29658f84011f072103f5d5103756b8da
maiko-y77 <122130876+maiko-y77@users.noreply.github.com>
2023-07-24 05:24:04 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+
+export const InputForm = ({ taskList, setTaskList }) => {
+
+    const [inputText, setInputText] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setTaskList([
+            ...taskList,
+            {
+                id: taskList.length,
+                text: inputText,
+                completed: false
+            }
+        ]);
+        setInputText("")
+    }
+
+    const handleChange = (e) => {
+        setInputText(e.target.value);
+    }
+
+    return (
+        <div className='inputForm'>
+            <form onSubmit={handleSubmit}>
+                <input type="text" onChange={handleChange} value={inputText} />
+                <button>
+                    <i className="bi bi-plus-square-fill"></i>
+                </button>
+            </form>
+        </div>
+    )
+}
